ba0bca856c13e228516bf5ddc15dfac2398da34c
Eric Lannert <elannert@icstars.org>
2017-01-19 16:57:49 +0000

    adding computing pmass level

--- g.sh
+++ g.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # use this once: git config credential.helper store
 git add *
-git commit -m "adding lag functions back in at outer level"
+git commit -m "adding computing pmass level"
 git push origin master
