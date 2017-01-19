3e83a8e230c89aba28ba8965b3aaeeb63d98e4c2
Eric Lannert <elannert@icstars.org>
2017-01-19 17:59:45 +0000

    incorporating flags for fusion and asm into users sql - not positive this is a good idea...

--- g.sh
+++ g.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # use this once: git config credential.helper store
 git add *
-git commit -m "adding main pidx query"
+git commit -m "incorporating flags for fusion and asm into users sql - not positive this is a good idea..."
 git push origin master
