46a459fb9a43d690837e1818f844a0c68cf8f2f4
Eric Lannert <elannert@icstars.org>
2017-01-19 16:45:08 +0000

    density v1 into main

--- g.sh
+++ g.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # use this once: git config credential.helper store
 git add *
-git commit -m "density v1 in scratch"
+git commit -m "density v1 into main"
 git push origin master
--- pidxhistory.sql
+++ pidxhistory.sql
@@ -198,3 +198,19 @@ from	(
 		on users_dates.pmonth = durationsql.monthending
 		and users_dates.user_id = durationsql.user_id
 
+        left outer join
+        (
+        select  max(density) as density, user_id, monthending
+        from    (
+                Select  count(link_id) over (partition by user_id order by created_at asc rows unbounded preceding) as density, user_id, date_trunc('month', created_at) as monthending 
+                From    (
+                        select min(created_at) as created_at, user_id, link_id 
+                        from user_registrations 
+                        group by link_id, user_id 
+                        Where link_type = 'ScheduledProgram'
+                        )
+                )
+        group by user_id, monthending        
+        ) densitysql
+        on users_dates.pmonth = densitysql.monthending
+		and users_dates.user_id = densitysql.user_id
