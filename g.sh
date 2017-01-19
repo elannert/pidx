55e9ad5dc7aecf47b3945b8a8134364493b8dcc8
Eric Lannert <elannert@icstars.org>
2017-01-19 15:54:38 +0000

    this stopped the [Amazon][RedShift ODBC] (30) Error occurred while trying to execute a query: ERROR: ORDER/GROUP BY expression not found in targetlist

--- g.sh
+++ g.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # use this once: git config credential.helper store
 git add *
-git commit -m "seed"
+git commit -m "this stopped the [Amazon][RedShift ODBC] (30) Error occurred while trying to execute a query: ERROR: ORDER/GROUP BY expression not found in targetlist"
 git push origin master
