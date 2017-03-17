375674737cb509af1d6245d4227f9d641e59cfe8
Eric Lannert <elannert@icstars.org>
2017-03-17 18:36:32 +0000

    tweak to pilot

--- g.sh
+++ g.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # use this once: git config credential.helper store
 git add *
-git commit -m "adding pilot students by claim code"
+git commit -m "tweak to pilot"
 git push origin master
