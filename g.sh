7ba1710d32aa6e78698416afd9ee0b968ff70928
Eric Lannert <elannert@icstars.org>
2017-04-24 21:01:48 +0000

    changed from claim codes to scheduled program ids because of risk from multiple claim codes like badge codes used by divas

--- g.sh
+++ g.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # use this once: git config credential.helper store
 git add *
-git commit -m "complete claim code list - with badge claim code used for divas BGL3U"
+git commit -m "changed from claim codes to scheduled program ids because of risk from multiple claim codes like badge codes used by divas"
 git push origin master
