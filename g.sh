013b9d05a77f733cceb9e3500e14abd62e4fc341
Eric Lannert <elannert@icstars.org>
2017-04-24 15:56:30 +0000

    complete claim code list - with badge claim code used for divas BGL3U

--- g.sh
+++ g.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # use this once: git config credential.helper store
 git add *
-git commit -m "complete claim code list"
+git commit -m "complete claim code list - with badge claim code used for divas BGL3U"
 git push origin master
